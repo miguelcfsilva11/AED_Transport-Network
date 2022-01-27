3d63a4bc30baf0c8055911915737417c75016722
Mariana <up202005658@up.pt>
2022-01-27 14:55:10 +0000

    Funcao de distancia pronta

--- src/graph/transport_network.cpp
+++ src/graph/transport_network.cpp
@@ -1,4 +1,25 @@
 #include "transport_network.h"
+#include <math.h>
+
+
+float TransportNetwork::distanceFunc(string& code1, string& code2)
+{
+    int a,b;
+    float lat1, lat2, long1, long2, distance;
+    a = findStop(code1);
+    b = findStop(code2);
+    lat1 = g.getStops()[a].latitude;
+    long1 = g.getStops()[a].longitude;
+    lat2 = g.getStops()[b].latitude;
+    long2 = g.getStops()[b].longitude;
+
+    distance = 2*6371*asin(sqrt(pow(sin((lat2-lat1)/2),2)+cos(lat1)*cos(lat2)*pow(sin((long2-long1)/2),2)));
+    return distance;
+
+}
+
+
+
 
 void TransportNetwork::readStops(string& filename)
 {

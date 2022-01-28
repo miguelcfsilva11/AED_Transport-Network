ef64076ddedeed9be53df3ada4c02cea32848282
Martim <mitram790@gmail.com>
2022-01-28 15:04:48 +0000

    BFS and Graph is working

--- src/graph.cpp
+++ src/graph.cpp
@@ -2,51 +2,84 @@
 // Pedro Ribeiro (DCC/FCUP) [09/01/2022]
 
 #include "graph.h"
+#include <queue>
 
 // Constructor: nr nodes and direction (default: undirected)
-Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num+1) {
+Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num + 1)
+{
 }
 
 // Add edge from source to destination with a certain weight
-void Graph::addEdge(int src, int dest, int weight) {
-    if (src<1 || src>n || dest<1 || dest>n) return;
+void Graph::addEdge(int src, int dest, int weight)
+{
+    if (src < 0 || src > n-1 || dest < 0 || dest > n-1)
+        return;
     nodes[src].adj.push_back({dest, weight});
-    if (!hasDir) nodes[dest].adj.push_back({src, weight});
+    if (!hasDir)
+        nodes[dest].adj.push_back({src, weight});
 }
 
-void Graph::addStop(Stop& stop)
-{
-    stops.push_back(stop);
-}
-
-int Graph::findStop(Stop& stop)
-{
-    for (size_t i = 0; i < stops.size(); i++)
-    {
-        if (stop.code == stops[i].code)
-            return i;
-    }
-
-    return -1;
-}
-
-vector<Stop>&Graph::getStops()
-{
-    return stops;
-}
 // ----------------------------------------------------------
 // Exercicio 3: Algoritmo de Prim
 // ----------------------------------------------------------
 // TODO
-int Graph::prim(int r) {
+int Graph::prim(int r)
+{
     return 0;
 }
-
 
 // ----------------------------------------------------------
 // Exercicio 5: Algoritmo de Kruskal
 // ----------------------------------------------------------
 // TODO
-int Graph::kruskal() {
+int Graph::kruskal()
+{
     return 0;
+}
+
+
+void Graph::setStops(vector<Stop>& stops){
+    this->stops = stops;
+}
+int Graph::getStopIndex(string& code){
+    for(int i = 0 ; i< stops.size(); i++)
+        if(stops[i].code == code)
+            return i;
+    
+    cout << "Trying to Find "<< code << " That code does not exist!!\n";
+    
+    return -1;
+}
+
+
+void Graph::BFS(int startingNode)
+{
+    unordered_set<int> visitedNodes;
+    queue<int> to_visitNodes;
+    to_visitNodes.push(startingNode);
+    visitedNodes.insert(startingNode);
+    while (to_visitNodes.size())
+    {
+        int currentNode = to_visitNodes.front();
+        to_visitNodes.pop();
+        cout << stops[currentNode].code  << "->" ;
+        //cout << nodes[currentNode].adj.size();
+        
+        for (Edge g : nodes[currentNode].adj)
+        {
+            if (visitedNodes.find(g.dest) == visitedNodes.end())
+            {
+                to_visitNodes.push(g.dest);
+                visitedNodes.insert(g.dest);
+            }   
+        }
+    }
+}
+
+void Graph::printAdjancies(int node){
+   cout << stops[node].code << " Has connections to :\n";
+   for(Edge e :nodes[node].adj){
+       cout<< stops[e.dest].code << ", ";
+   }
+
 }
--- src/graph.h
+++ src/graph.h
@@ -22,7 +22,6 @@ class Graph {
     };
 
     struct Node {
-
         list<Edge> adj;     //The list of outgoing edges (to adjacent nodes)
 
     };
@@ -31,7 +30,7 @@ class Graph {
     int n;              // Graph size (vertices are numbered from 1 to n)
     bool hasDir;        // false: undirect; true: directed
     vector<Node> nodes;
-    vector<Stop> stops; // The list of nodes being represented
+    vector<Stop> stops;
 
 public:
     // Constructor: nr nodes and direction (default: undirected)
@@ -39,15 +38,18 @@ public:
 
     // Add edge from source to destination with a certain weight
     void addEdge(int src, int dest, int weight = 1);
-    void addStop(Stop& stop);
-
-    int findStop(Stop& stop);
-
-    vector<Stop>& getStops();
 
     // ----- Functions to implement in this class -----
     int prim(int v);
     int kruskal();
+
+    void BFS(int startingNode);
+    vector<Stop>& getStops();
+    void setStops(vector<Stop>& stop);
+    int getStopIndex(string &stop_code);
+    void printAdjancies(int node );
+    
 };
+
 
 #endif
